037b3419d4d5bef244b6a3b0b251b0cb70828d6b
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2022-03-29 17:01:16 +0200

    Change version to 0.2.2.0, update changelog

--- src/Ray1Editor/ViewModels/AppViewModel.cs
+++ src/Ray1Editor/ViewModels/AppViewModel.cs
@@ -66,7 +66,7 @@ public class AppViewModel : BaseViewModel
     /// <summary>
     /// The current app version
     /// </summary>
-    public Version CurrentAppVersion => new Version(0, 2, 1, 0);
+    public Version CurrentAppVersion => new Version(0, 2, 2, 0);
 
     /// <summary>
     /// Indicates if the current version is a BETA version
